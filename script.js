7723d480af56c8d6fb76becade8e44f183cdc226
Joseph Robert Vann <143433093+R0bb1eV@users.noreply.github.com>
2025-06-19 08:39:44 -0400

    fxed issues with adding task and set limit to char length

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
 function addTask() {
+  let task = inputBox.value;
   if(inputBox.value === '') {
     alert("Add a task!")
+  } else if(task.length > 32) {
+    alert("This task name is too long!")
   } else {
     let li = document.createElement("li");
     li.innerHTML = inputBox.value;
@@ -16,13 +19,13 @@ function addTask() {
   saveData();
 }
 
-listContainer.addEventListener("click", function(e) {
-  if(e.target.tagName === "LI") {
-    e.target.calssList.toggle("checked");
+listContainer.addEventListener("click", function(event) {
+  if(event.target.tagName === "LI") {
+    event.target.classList.toggle("checked");
     saveData();
   }
-  else if (e.target.tagName === "SPAN") {
-    e.target.parentElement.remove();
+  else if (event.target.tagName === "SPAN") {
+    event.target.parentElement.remove();
     saveData();
   }
 }, false);
@@ -35,4 +38,13 @@ function showTask() {
 }
 
 showTask();
+
+document.getElementById("input-box", addEventListener("keydown", function(event) {
+  if(event.key === "Enter") {
+    document.getElementById("task-button").click();
+  }
+}));
+
+
+
  
